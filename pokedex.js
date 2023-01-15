08d842c4d5ff38aeab04c5492a13813a8868eddd
Melissa Ridente <melissaridente3e@gmail.com>
2023-01-15 14:38:15 +0100

    first-commit

--- pokedex.js
+++ pokedex.js
@@ -57,5 +57,24 @@ const final = async () => {
   })
 }
 
-// eventos()
+
+  // const pulsor = () => {
+  //   console.log('si')
+  // }
+  // divCard.addEventListener('click' , pulsor)
+
+// const filtrar = async () => {
+//   const datos = await obtenerDatos()
+//   let valor = input.value.toLowerCase()
+//   const encontrado = datos.filter(pokemon => pokemon.name.toLowerCase().includes(valor))
+//     console.log(encontrado)
+//     let pantallaNueva = document.createElement('div')
+//     pantallaNueva.innerText('HOLA')
+//     // pantallaNueva.innerHTML=encontrado;
+//     card.append(pantallaNueva)
+// }
+
+// input.addEventListener('input', filtrar);
+
+
 final()
